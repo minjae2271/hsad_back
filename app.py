472019ce870fd22315a9c91cba55c3ded816a11e
minjae <minjae2271@gmail.com>
2024-12-19 12:48:43 +0100

    arg

--- app.py
+++ app.py
@@ -36,6 +36,8 @@ def engine():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.add_argument('--disable-software-rasterizer')
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36")
 
     service = Service(ChromeDriverManager().install())
